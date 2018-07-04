abadaf3caa3c18168ffac51af3841d1f1d688b91
DESKTOP-9K7D7EG\Gandy <gandoy77@gmail.com>
2018-07-04 02:39:32 -0300

    Menu, animacion del pterodactilo, y un par de cosas mas.

---
+++ Proyecto2/Assets/Texturas/Menu/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void Nivel1()
+    {
+        SceneManager.LoadScene("Nivel1");
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
