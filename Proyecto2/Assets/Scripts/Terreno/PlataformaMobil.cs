9d5378de9f5d1e1ddb994ca775ee42108afa8d41
Nicolas Carella <esep642@gmail.com>
2018-07-13 12:33:49 -0300

    plataformas mobiles

---
+++ Proyecto2/Assets/Scripts/Terreno/PlataformaMobil.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMobil : MonoBehaviour {
+
+    [SerializeField]
+    private List<Transform> Waypoints;
+    [SerializeField]
+    private int CurrentWaypoint;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float PauseTime;
+    private float LastPause;
+
+	
+	void Update () {
+        if(LastPause<Time.time)
+        transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentWaypoint].position, speed*Time.deltaTime);
+        if (Vector3.Distance(transform.position, Waypoints[CurrentWaypoint].position) < speed*Time.deltaTime)
+        {
+            transform.position = Waypoints[CurrentWaypoint].position;
+            CurrentWaypoint++;
+            CurrentWaypoint = CurrentWaypoint % Waypoints.Count; //aritmetica modular vieja
+            LastPause = Time.time + PauseTime;
+           
+        }
+    }
+}
