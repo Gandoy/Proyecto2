0a20622639848f8330f734d395f1a153f27db876
Esep01 <esep642@gmail.com>
2019-10-15 07:01:05 -0300

    Visual Queue al hacer daño, implementado al boss 1 (triceratops azul)

--- Proyecto2/Assets/Scripts/Misc/Hurtbox.cs
+++ Proyecto2/Assets/Scripts/Misc/Hurtbox.cs
@@ -5,10 +5,47 @@ using UnityEngine;
 public class Hurtbox : MonoBehaviour {
     [SerializeField]
     private Character Char;
+    [SerializeField]
+    private GameObject gameobject;
+    public bool flashes;
+    
+   // public float FlashDuration;
+    public float Flashtime;
+    public float Flashes;
+    
 
   public virtual void GetHit(int damage)
     {
        // Debug.Log("entroGetHit");
         Char.GetDamaged(damage);
+        if (flashes)
+        {
+            DamageFlash();
+            //DamageFlashStart();
+        }
+       
+    }
+   /* private void Update()
+    {
+        if (true)
+        {
+            DamageFlash();
+        }
+    }*/
+   /* private void DamageFlashStart()
+    {
+     InvokeRepeating("DamageFlash", 0, Flashtime);
+    }*/
+    private void DamageUnFlash()
+    {
+        SkinnedMeshRenderer mesh = gameobject.GetComponent<SkinnedMeshRenderer>();
+        mesh.enabled = true;
+    }
+    private void DamageFlash()
+    {
+        SkinnedMeshRenderer mesh = gameobject.GetComponent<SkinnedMeshRenderer>();
+        mesh.enabled = false;
+        Invoke("DamageUnFlash", Flashtime);
     }
 }
+
