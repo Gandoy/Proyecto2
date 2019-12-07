43feaa7870384dea6c1a03febad8ea38b49dcc9e
Gandoy <gandoy77@gmail.com>
2019-12-07 17:09:23 -0300

    Interfaz.

---
+++ Proyecto2/Assets/Menu/MenuPausa.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public static bool JuegoPausado = false;
+
+    public GameObject MenuPausaUI;
+
+    public Animator anim;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (JuegoPausado)
+            {
+                Continuar();
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+    public void Continuar()
+    {
+        MenuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        JuegoPausado = false;
+    }
+    void Pausa()
+    {
+        MenuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        JuegoPausado = true;
+    }
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
