b67814e7a82d5060d6baf5ed772da42ca3d9f4c5
Esep01 <esep642@gmail.com>
2020-01-03 21:16:47 -0300

    Implementacion de nuevo script de movimiento de camara en nivel 1-1, falta tantear numeros.

---
+++ Proyecto2/Assets/Scripts/Misc/SmoothCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+
+{
+
+    [SerializeField]
+    private float SmoothSpeed;
+    [SerializeField]
+    private Vector3 offset;
+
+
+    
+    private GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");   
+    }
+
+    // Update is called once per frame
+    
+    void LateUpdate ()
+    {
+        Vector3 DesiredPosition = offset + Player.transform.position;
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed);
+        transform.position = SmoothedPosition;
+    }
+}
