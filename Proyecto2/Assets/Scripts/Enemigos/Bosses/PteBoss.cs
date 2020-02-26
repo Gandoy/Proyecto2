0c5fcda3b91294fa4c2059c745edc3cd77e2bedc
Esep01 <esep642@gmail.com>
2020-02-26 04:05:21 -0300

    Mejorado Boss 3 para hacer un bombardeo de lado a lado despues de completar su patron de ataque original.

--- Proyecto2/Assets/Scripts/Enemigos/Bosses/PteBoss.cs
+++ Proyecto2/Assets/Scripts/Enemigos/Bosses/PteBoss.cs
@@ -11,12 +11,37 @@ public class PteBoss : MurcielagoIA {
     [SerializeField]
     private List<int> PlatTargets;
     public string Escena;
-    
-    
+    [SerializeField]
+    private GameObject Projectile;
+    [SerializeField]
+    private List <int> StoneDroppingWaypoints;
+    [SerializeField]
+    private float ProjectileCD;
+    private float lastProjectileFired;
+    [SerializeField]
+    private List<int> WaypointsToTurnLeft;
+    [SerializeField]
+    private List<int> WaypointsToTurnRight;
+    [SerializeField]
+    private Transform DropSpot;
+
+    private void DropStone()
+    {
+        if (StoneDroppingWaypoints.Contains(CurrentWaypoint) && ProjectileCD + lastProjectileFired < Time.time)
+        {
+            Instantiate(Projectile, DropSpot.position, DropSpot.rotation);
+            lastProjectileFired = Time.time;
+        }
+           
+    }
+
     private void Update()
     {
         if (LastPause < Time.time)
+        {
             transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentWaypoint].position, speed * Time.deltaTime);
+            DropStone();
+        }
         if (Vector3.Distance(transform.position, Waypoints[CurrentWaypoint].position) < speed * Time.deltaTime)
         {
             transform.position = Waypoints[CurrentWaypoint].position;
@@ -26,10 +51,10 @@ public class PteBoss : MurcielagoIA {
             if (Plats[CurrentWaypoint] != null) 
             Plats[CurrentWaypoint].Mov(PlatTargets[CurrentWaypoint]);
             // Rotator.turnTowards(Waypoints[CurrentWaypoint].position);
-            //fix para la rotacion del boss, hardcodie en que waypoints rota<--------
-            if (CurrentWaypoint == 1) Rotator.turnleft();
-            if (CurrentWaypoint == 6) Rotator.turnright();
-           //fin del fix, parametrizar esta mierda si algun dia hay que fixxearlo, programe esto a las 3 am, no me juzgues.
+            
+            if (WaypointsToTurnLeft.Contains(CurrentWaypoint)) Rotator.turnleft();
+            if (WaypointsToTurnRight.Contains(CurrentWaypoint)) Rotator.turnright();
+
             LastPause = Time.time + PauseTime;
             //Rotator.turnTowards(Waypoints[CurrentWaypoint].position);
 
