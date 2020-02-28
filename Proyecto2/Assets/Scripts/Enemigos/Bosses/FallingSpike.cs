ccf84ff88bf1b3c9cd2d03376d2bb42b0140def5
Esep01 <esep642@gmail.com>
2020-02-27 23:05:41 -0300

    Creado el Prefab del spike spawner para el boss 1, falta colocarlos y darle una forma al boss de activarlos

---
+++ Proyecto2/Assets/Scripts/Enemigos/Bosses/FallingSpike.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpike : MonoBehaviour
+{
+    [SerializeField]
+    private float OpeningSpeed;
+    [SerializeField]
+    private float FinalSpeed;
+    [SerializeField]
+    private float Acceleration;
+    [SerializeField]
+    private float DeleteTimer;
+    private float Spawntime;
+    private float Speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawntime = Time.time;
+        Speed = OpeningSpeed;
+    }
+
+    private void Physics()
+    {
+        if (Speed >= FinalSpeed)
+            Speed += Acceleration;
+        else if (Speed < FinalSpeed)
+            Speed = FinalSpeed;
+
+        transform.Translate(0,Speed * Time.deltaTime,0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > DeleteTimer + Spawntime)
+            Destroy(gameObject);
+        Physics();
+    }
+}
---
+++ Proyecto2/Assets/Scripts/Terreno/SpikeSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Spike;
+
+    public void SpawnSpike()
+    {
+        Instantiate(Spike, transform.position, transform.rotation);
+    }
+
+}
