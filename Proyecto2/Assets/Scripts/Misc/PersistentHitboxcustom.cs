2e8398a46780cb865a02ddea2522fc9b264bb6e4
Esep01 <esep642@gmail.com>
2020-03-05 09:48:10 -0300

    tomahawk implementado, ver brillo en un futuro

---
+++ Proyecto2/Assets/Scripts/Misc/PersistentHitboxcustom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentHitboxcustom : MonoBehaviour
+{
+    [SerializeField]
+    private int dmg;
+    private void OnTriggerStay(Collider other)
+    {
+        Hurtbox Damaged;
+        Damaged = other.GetComponent<Hurtbox>();
+        Damaged.GetHit(dmg);
+
+    }
+}
