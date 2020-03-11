8768f50641d73ec98b3cd2ae249f8b69725afd40
Esep01 <esep642@gmail.com>
2020-03-10 21:34:10 -0300

    controles reposicionados, intro y creditos implementados con script de timer, tutorial tutorializado, falta hacer que el boss 1 prenda el boton de cambiar arma

---
+++ Proyecto2/Assets/Scripts/Misc/CambiarEscenaTimed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscenaTimed : MonoBehaviour
+{
+    [SerializeField]
+    private string Escena;
+    [SerializeField]
+    private float delay;
+    void Start()
+    {
+        Invoke("Cambio", delay);
+    }
+    //este script no esta asociado con el pro
+    private void Cambio()
+    {
+        SceneManager.LoadScene(Escena);
+
+    }
+
+
+}
---
+++ Proyecto2/Assets/Scripts/Misc/InOutSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InOutSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> Things;
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(GameObject x in Things)
+        {
+            x.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject x in Things)
+        {
+            x.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
